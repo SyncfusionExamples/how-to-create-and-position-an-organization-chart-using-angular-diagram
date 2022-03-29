d2a7a6cbddd288d18d11c80c6357bd8baf666808
subaa1492 <60738126+subaa1492@users.noreply.github.com>
2022-03-29 21:15:07 +0530

    Added data binding sample for diagram

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { ConnectorModel, DataBinding, Diagram, HierarchicalTree, LayoutModel,
+NodeModel, SnapConstraints, SnapSettingsModel } from '@syncfusion/ej2-angular-diagrams';
+import {DataManager, Query} from '@syncfusion/ej2-data';
+Diagram.Inject(DataBinding, HierarchicalTree);
+export interface EmployeeInfo {
+  Name: string
+}
+export interface DataInfo {
+  [key : string]: string
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-diagram';
+  public RemoteDataSource: Object = {
+    id: 'EmployeeID',
+    parentId: 'ReportsTo',
+    dataSource: new DataManager(
+      { url: 'https://mvc.syncfusion.com/Services/Northwnd.svc/', crossDomain: true },
+      new Query().from('Employees').select('EmployeeID,ReportsTo,FirstName').take(9)
+    )
+
+  };
+  public RemoveGridLines: SnapSettingsModel = { constraints: SnapConstraints.None};
+  public defaultConnectorValues(defaultconnector: ConnectorModel): ConnectorModel {
+    defaultconnector.type = "Orthogonal";
+    defaultconnector.style = { strokeColor: '#6f409f', strokeWidth: 2 };
+    defaultconnector.targetDecorator = { style : { fill: '#6f409f', strokeColor: '#6f409f' }};
+    return defaultconnector;
+
+  }
+  public defaultNodeValues(defaultnode: NodeModel): NodeModel {
+    defaultnode.height = 50;
+    defaultnode.width = 100;
+    defaultnode.annotations = [
+      { content: (defaultnode.data as EmployeeInfo).Name, style: { color: "white" }}
+      //{ content: (defaultnode.data as DataInfo)['FirstName'], style: { color: "white" }}
+    ];
+    defaultnode.style = { fill: '#048785', strokeColor: 'Transparent', strokeWidth: 1  };
+    return defaultnode;
+
+  }
+  public layoutSettings: LayoutModel = {
+    type: "OrganizationalChart"
+  };
+  public employeeData: Object[] = [
+    { Name: "Elizabeth", Role: "Director" },
+    { Name: "Christina", ReportingPerson: "Elizabeth", Role: "Manager" },
+    { Name: "Yoshi", ReportingPerson: "Christina", Role: "Lead" },
+    { Name: "Philip", ReportingPerson: "Christina", Role: "SalesExecutive" },
+    { Name: "John", ReportingPerson: "Yoshi", Role: "Engineer" },
+    { Name: "James", ReportingPerson: "Yoshi", Role: "Engineer" },
+    { Name: "Robert", ReportingPerson: "Christina", Role: "Lead" },
+    { Name: "David", ReportingPerson: "Robert", Role: "Engineer" }, 
+    { Name: "Yang", ReportingPerson: "Elizabeth", Role: "Manager" },
+    { Name: "Roland", ReportingPerson: "Yang", Role: "Lead" },
+    { Name: "Paul", ReportingPerson: "Roland", Role: "Engineer" },
+    { Name: "Yvonne", ReportingPerson: "Yang", Role: "Lead" },
+    { Name: "Jack", ReportingPerson: "Yvonne", Role: "Engineer" }
+  ];
+
+  public jsonDatasourceSettings: Object = {
+    id: "Name",
+    parentId: "ReportingPerson",
+    dataSource: new DataManager(this.employeeData as JSON[])
+  };
+  
+ }
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DiagramModule, SnappingService } from '@syncfusion/ej2-angular-diagrams'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DiagramModule,
+    AppRoutingModule
+  ],
+  providers: [SnappingService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
